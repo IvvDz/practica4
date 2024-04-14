ecce8c4e6d2b5c65a4909f4cbb64ed9a4cfd93df
Ivan <ivan.diazmarcos@usp.ceu.es>
2024-04-14 18:55:08 +0200

    terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,19 +1,15 @@
 package aplicacion;
 
-import mates.Matematicas;
-import maths.CalculadoraDistancias;
+import palabras.CalculadoraDistancias;
 
-public class Principal {
+public class Principal { 
     public static void main(String[] args) {
         // Definimos dos palabras para calcular la distancia de edición
         String palabra1 = "casa";
         String palabra2 = "caja";
 
-        // Calculamos y mostramos la distancia de edición utilizando la implementación de Matematicas.distance
-        System.out.println("Distancia de edición entre '" + palabra1 + "' y '" + palabra2 + "' (usando Matematicas.distance): " + Matematicas.distance(palabra1, palabra2));
-
         // Calculamos y mostramos la distancia de edición utilizando la implementación de CalculadoraDistancias.calcularDistancia
-        System.out.println("Distancia de edición entre '" + palabra1 + "' y '" + palabra2 + "' (usando CalculadoraDistancias.calcularDistancia): " + CalculadoraDistancias.calcularDistancia(palabra1, palabra2));
+        System.out.println("Distancia de edición entre '" + palabra1 + "' y '" + palabra2 + "': " + CalculadoraDistancias.calcularDistancia(palabra1, palabra2));
     }
 }
 
